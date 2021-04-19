80cb64f6a7f30db39e0dd8257b9f93cfc07ab762
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2021-04-19 18:38:54 -0300

    feat: gather new meetup input values and fix labels

--- src/components/Meetups/NewMeetupForm/NewMeetupForm.js
+++ src/components/Meetups/NewMeetupForm/NewMeetupForm.js
@@ -1,30 +1,55 @@
+import { useRef } from 'react';
 import Card from '../../UI/Card/Card';
 import styles from './NewMeetupForm.module.css';
 
-const NewMeetupForm = () => (
-    <Card>
-        <form className={styles.form}>
-            <div className={styles.control}>
-                <label htmlFor="title">Meetup Title</label>
-                <input type="text" required id="title" />
-            </div>
-            <div className={styles.control}>
-                <label htmlFor="image">Meetup Title</label>
-                <input type="url" required id="image" />
-            </div>
-            <div className={styles.control}>
-                <label htmlFor="address">Meetup Title</label>
-                <input type="text" required id="address" />
-            </div>
-            <div className={styles.control}>
-                <label htmlFor="description">Meetup Title</label>
-                <textarea id="description" required rows="5"></textarea>
-            </div>
-            <div className={styles.actions}>
-                <button>Add Meetup</button>
-            </div>
-        </form>
-    </Card>
-);
+const NewMeetupForm = () => {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const title = titleInputRef.current.value;
+        const image = imageInputRef.current.value;
+        const address = addressInputRef.current.value;
+        const description = descriptionInputRef.current.value;
+
+        const meetupData = { title, image, address, description };
+
+        console.log(meetupData);
+    };
+
+    return (
+        <Card>
+            <form className={styles.form} onSubmit={submitHandler}>
+                <div className={styles.control}>
+                    <label htmlFor="title">Meetup Title</label>
+                    <input type="text" required id="title" ref={titleInputRef} />
+                </div>
+                <div className={styles.control}>
+                    <label htmlFor="image">Meetup Image</label>
+                    <input type="url" required id="image" ref={imageInputRef} />
+                </div>
+                <div className={styles.control}>
+                    <label htmlFor="address">Meetup Address</label>
+                    <input type="text" required id="address" ref={addressInputRef} />
+                </div>
+                <div className={styles.control}>
+                    <label htmlFor="description">Meetup Description</label>
+                    <textarea
+                        id="description"
+                        rows="5"
+                        required
+                        ref={descriptionInputRef} ></textarea>
+                </div>
+                <div className={styles.actions}>
+                    <button>Add Meetup</button>
+                </div>
+            </form>
+        </Card>
+    );
+};
 
 export default NewMeetupForm;
