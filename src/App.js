e9ff35381644a9500eba6309d134e781391b8113
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2021-04-19 17:17:07 -0300

    feat: extract TodoList component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import { useState } from 'react';
-import Todo from './components/Todo/Todo';
+import TodoList from './components/TodoList/TodoList';
 
 const App = () => {
-  const [todos, setTodos] = useState(['First Todo', 'Second Todo', 'Third Todo']);
-
-  const deleteTodoConfirmedHandler = (deletedTodoIndex) => {
-    const allTodos = [...todos];
-    allTodos.splice(deletedTodoIndex, 1);
-    setTodos(allTodos);
-  };
-
   return (
     <div>
-      <h1>My Todos</h1>
-      {todos.map((todo, index) => (
-        <Todo key={index} text={todo} deleted={() => deleteTodoConfirmedHandler(index)} />
-      ))}
+      <TodoList />
     </div>
   );
 };
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Todo from './components/Todo/Todo';
+
+const TodoList = () => {
+    const [todos, setTodos] = useState(['First Todo', 'Second Todo', 'Third Todo']);
+
+    const deleteTodoConfirmedHandler = (deletedTodoIndex) => {
+        const allTodos = [...todos];
+        allTodos.splice(deletedTodoIndex, 1);
+        setTodos(allTodos);
+    };
+
+    return (
+        <div>
+            <h1>My Todos</h1>
+            {todos.map((todo, index) => (
+                <Todo key={index} text={todo} deleted={() => deleteTodoConfirmedHandler(index)} />
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
