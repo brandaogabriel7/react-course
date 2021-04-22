ce7c3b53b5f718cfd347f637500b9f87e65f2d52
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2021-04-22 11:56:58 -0300

    feat: add badge in main navigation to indicate total favorites

--- src/components/Layout/MainNavigation/MainNavigation.js
+++ src/components/Layout/MainNavigation/MainNavigation.js
@@ -1,18 +1,28 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import FavoritesContext from '../../../contexts/favorites-context';
 
 import styles from './MainNavigation.module.css';
 
-const MainNavigation = () => (
-    <header className={styles.header}>
-        <div className={styles.logo}>React Meetups</div>
-        <nav>
-            <ul>
-                <li><NavLink exact activeClassName={styles.active} to="/meetups">All meetups</NavLink></li>
-                <li><NavLink activeClassName={styles.active} to="/meetups/new">New meetup</NavLink></li>
-                <li><NavLink activeClassName={styles.active} to="/meetups/favorites">Favorite meetups</NavLink></li>
-            </ul>
-        </nav>
-    </header>
-);
+const MainNavigation = () => {
+    const favoritesContext = useContext(FavoritesContext);
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>React Meetups</div>
+            <nav>
+                <ul>
+                    <li><NavLink exact activeClassName={styles.active} to="/meetups">All meetups</NavLink></li>
+                    <li><NavLink activeClassName={styles.active} to="/meetups/new">New meetup</NavLink></li>
+                    <li>
+                        <NavLink activeClassName={styles.active} to="/meetups/favorites">
+                            Favorite meetups
+                        <span className={styles.badge}>{favoritesContext.total}</span>
+                        </NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default MainNavigation;
